24e613dd09b143de73a2d7d5458f5efa85d7b981
Yuannan Cai <caiyuannan@gmail.com>
2021-12-29 13:21:14 -0800

    Update wildcard-matching.cpp

--- wildcard-matching/wildcard-matching.cpp
+++ wildcard-matching/wildcard-matching.cpp
@@ -3,20 +3,19 @@ public:
     bool isMatch(string s, string p) {
       // match[i][j] == true iff s.substr[0, i) matches p.substr[0, j)
       std::vector<std::vector<bool>> match(s.size() + 1, std::vector<bool>(p.size() + 1, false));
-      std::vector<std::vector<bool>> match_one_i(s.size() + 1, std::vector<bool>(p.size() + 1, false));
-      // std::vector<bool> match_one_i(p.size() + 1, false);
 
-      for (int i = 0; i <= s.size(); i++) {
-        for (int j = 0; j <= p.size(); j++) {        
+      for (int j = 0; j <= p.size(); j++) {  
+        bool match_one_i = false;
+        for (int i = 0; i <= s.size(); i++) {
           if (i == 0 && j == 0) {
             match[i][j] = true;
-            match_one_i[i][j] = true;
+            match_one_i |= match[i][j];
             continue;
           }
           
           if (j == 0 /* i != 0*/) {
             match[i][j] = false;
-            match_one_i[i][j] = true;
+            match_one_i |= match[i][j];
             continue;
           }
 
@@ -24,7 +23,7 @@ public:
 
           if (i == 0 /* && j != 0*/) {
             match[i][j] = match[i][j - 1] && p_char == '*';
-            match_one_i[i][j] = match[i][j];
+            match_one_i |= match[i][j];
             continue;
           }
           
@@ -32,15 +31,16 @@ public:
           
           if (p_char == '*') {
             // matches zero chars
-            match[i][j] = match[i][j - 1] | match_one_i[i - 1][j];
             // matches one or more chars
-            /* any true in match[0 - i)[j]*/
+            // any true in match[0 - i)[j]
+            match[i][j] = match[i][j - 1] | match_one_i;
+
           } else if (p_char == '?') {
             match[i][j] = match[i - 1][j - 1];
           } else {
             match[i][j] = match[i - 1][j - 1] && s_char == p_char;
           }
-          match_one_i[i][j] = match_one_i[i - 1][j] | match[i][j];
+          match_one_i |= match[i][j];
         }
       }
       return match[s.size()][p.size()];
