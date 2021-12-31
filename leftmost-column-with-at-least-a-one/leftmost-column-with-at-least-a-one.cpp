8840087a643d1bb30a1cfdbf33c6dde0ba5665af
Yuannan Cai <caiyuannan@gmail.com>
2021-12-31 14:37:16 -0800

    Update leftmost-column-with-at-least-a-one.cpp

--- leftmost-column-with-at-least-a-one/leftmost-column-with-at-least-a-one.cpp
+++ leftmost-column-with-at-least-a-one/leftmost-column-with-at-least-a-one.cpp
@@ -12,12 +12,29 @@ class Solution {
 public:
     int leftMostColumnWithOne(BinaryMatrix &m) {
         // # of 1 is ascending
-        return BinarySearchColumns(m, 1, 0, m.dimensions()[1] - 1);
+        // return BinarySearchColumns(m, 1, 0, m.dimensions()[1] - 1);
+        
+        int row = 0;
+        int col = m.dimensions()[1] - 1;
+        int left_most = m.dimensions()[1];
+        while (row < m.dimensions()[0] && col >= 0) {
+            if (m.get(row, col) == 1) {
+                left_most = col;
+                col--;
+            } else {
+                row++;
+            }
+        }
+        
+        if (left_most == m.dimensions()[1]) {
+            return -1;
+        }
+        
+        return left_most;
     }
     
 private:
     int BinarySearchColumns(BinaryMatrix& m, int target, int begin_col, int end_col) {
-        cout << "begin " << begin_col << "end " << end_col << endl;
         if (begin_col > end_col) {
             return -1;
         }
@@ -33,13 +50,7 @@ private:
             return begin_col;
         }
         
-        // if (ones > target) {
-            return BinarySearchColumns(m, target, begin_col, mid_col);
-        // }
-        
-        
-        
-        // return BinarySearchColumns(m, target, begin_col, mid_col);
+        return BinarySearchColumns(m, target, begin_col, mid_col);         
     }
     int OnesInColumn(BinaryMatrix &m, int col) {
         int ones = 0;
