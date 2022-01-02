a0dbc57cfd4b98f525f190faae15688ad8c05273
Yuannan Cai <caiyuannan@gmail.com>
2022-01-01 20:20:02 -0800

    Time: 3 ms (12.14%), Space: 5.7 MB (96.39%) - LeetHub

---
+++ powx-n/powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        
+        if (n == 1) {
+            return x;
+        }
+        
+        if (n == -1) {
+            return 1 / x;
+        }
+        
+        double half = myPow(x, n / 2);
+        double remain = myPow(x, n % 2);
+        return half * half * remain;
+    }
+};
