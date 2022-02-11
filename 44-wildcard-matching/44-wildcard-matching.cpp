5aa6335aaf179ed4b7971b4ed6445898b4132c5c
Yuannan Cai <caiyuannan@gmail.com>
2022-02-11 01:20:22 -0800

    Time: 152 ms (23.51%), Space: 11.1 MB (55.79%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -5,17 +5,14 @@ public:
       std::vector<std::vector<bool>> match(s.size() + 1, std::vector<bool>(p.size() + 1, false));
 
       for (int j = 0; j <= p.size(); j++) {  
-        // bool match_one_i = false;
         for (int i = 0; i <= s.size(); i++) {
           if (i == 0 && j == 0) {
             match[i][j] = true;
-            // match_one_i |= match[i][j];
             continue;
           }
           
           if (j == 0 /* i != 0*/) {
             match[i][j] = false;
-            // match_one_i |= match[i][j];
             continue;
           }
 
@@ -23,7 +20,6 @@ public:
 
           if (i == 0 /* && j != 0*/) {
             match[i][j] = match[i][j - 1] && p_char == '*';
-            // match_one_i |= match[i][j];
             continue;
           }
           
@@ -33,14 +29,13 @@ public:
             // matches zero chars
             // matches one or more chars
             // any true in match[0 - i)[j]
-            match[i][j] = match[i][j - 1] | match[i - 1][j];// | match_one_i;
+            match[i][j] = match[i][j - 1] | match[i - 1][j];
 
           } else if (p_char == '?') {
             match[i][j] = match[i - 1][j - 1];
           } else {
             match[i][j] = match[i - 1][j - 1] && s_char == p_char;
           }
-          // match_one_i |= match[i][j];
         }
       }
       return match[s.size()][p.size()];
